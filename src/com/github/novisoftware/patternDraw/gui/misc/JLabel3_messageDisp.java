1e3bfb09cc7ead8d990cc7ea1d3bbf674b9a0a77
novisoftware <sh.sado@gmail.com>
2024-04-27 09:14:45 +0900

    パラメーター一覧の編集画面

---
+++ src/com/github/novisoftware/patternDraw/gui/misc/JLabel3_messageDisp.java
@@ -0,0 +1,29 @@
+package com.github.novisoftware.patternDraw.gui.misc;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JLabel;
+
+import com.github.novisoftware.patternDraw.utils.Preference;
+
+/**
+ * JLabelの見た目を変更したもの。
+ */
+public class JLabel3_messageDisp extends JLabel {
+	public JLabel3_messageDisp(String text) {
+		super(text);
+		this.setFont(Preference.MESSAGE_DISP_FONT);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+            RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+            RenderingHints.VALUE_RENDER_QUALITY);
+		super.paintComponent(g2);
+	}
+}
