155a3ec577ab0c99bef7578323a471e3d421e0a8
novisoftware <sh.sado@gmail.com>
2024-06-16 18:41:18 +0900

    jarの作成

--- devtool/mkjar.bat
+++ devtool/mkjar.bat
@@ -1,4 +1,1 @@
-REM jar --create --file jar/PatternDraw.jar --manifest ./META-INF/MANIFEST.MF  -C ./bin .
-REM jar --create --file jar/PatternDraw.jar --main-class com.github.novisoftware.patternDraw.gui.editor.guiMain.EditDiagramWindow -C ./bin .
-
-jar cfm jar/PatternDraw.jar ./META-INF/MANIFEST.MF -C ./bin .
+jar cfm jar/PatternDraw.jar ./manifest.txt -C ./bin .
