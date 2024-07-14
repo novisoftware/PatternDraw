9ba6c51cb638f9dd159e292b3a65098c7c80c8c7
novisoftware <sh.sado@gmail.com>
2024-07-14 15:22:35 +0900

    例外クラスの階層の説明を記述

--- src/com/github/novisoftware/patternDraw/core/exception/package-info.java
+++ src/com/github/novisoftware/patternDraw/core/exception/package-info.java
@@ -1,1 +1,16 @@
+/**
+ * クラス階層は以下。
+ * 
+ * <ul>
+ * <li> 《EvaluateException抽象クラス》
+ * <ul>
+ * <li> CaliculateException 計算にエラーが発生した場合に発生させる
+ * <li> 《ControlSignal抽象クラス》
+ * <ul>
+ * <li> BreakSignal ループのbreakで発生させる
+ * <li> ContinueSignal ループので発生させる
+ * </ul>
+ * </ul>
+ * </ul>
+ */
 package com.github.novisoftware.patternDraw.core.exception;
