b31587335ecb4c579beae6d84fecd0c7e2cf4d9a
novisoftware <sh.sado@gmail.com>
2024-07-14 12:25:06 +0900

    座標リストをzipする機能、concatする機能を追加

---
+++ src/com/github/novisoftware/patternDraw/geometricLanguage/lang/functions/def/pos/PosConcat.java
@@ -0,0 +1,67 @@
+package com.github.novisoftware.patternDraw.geometricLanguage.lang.functions.def.pos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.github.novisoftware.patternDraw.core.CaliculateException;
+import com.github.novisoftware.patternDraw.core.langSpec.functions.FunctionDefInterface;
+import com.github.novisoftware.patternDraw.core.langSpec.typeSystem.Value;
+import com.github.novisoftware.patternDraw.core.langSpec.typeSystem.Value.ValueType;
+import com.github.novisoftware.patternDraw.core.langSpec.typeSystem.ValuePosList;
+import com.github.novisoftware.patternDraw.geometricLanguage.entity.Pos;
+import com.github.novisoftware.patternDraw.geometricLanguage.lang.InstructionRenderer;
+
+public class PosConcat implements FunctionDefInterface {
+	public static final String NAME = "pos_concat";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "つなぐ";
+	}
+
+	@Override
+	public String getDescription() {
+		return "2つの座標の系列をつなぎます。";
+	}
+
+	@Override
+	public ValueType[] getParameterTypes() {
+		ValueType[] ret = {ValueType.POS_LIST, ValueType.POS_LIST};
+		return ret;
+	}
+
+	@Override
+	public String[] getParameterNames() {
+		String[] ret = {"positions1", "positions2"};
+		return ret;
+	}
+
+	@Override
+	public String[] getParameterDescs() {
+		String[] ret = {"系列1", "系列2"};
+		return ret;
+	}
+
+	@Override
+	public ValueType getReturnType() {
+		return ValueType.POS_LIST;
+	}
+
+	@Override
+	public Value exec(List<Value> param, InstructionRenderer t) throws CaliculateException {
+		ArrayList<Pos> posList1 = Value.getPosList(param.get(0));
+		ArrayList<Pos> posList2 = Value.getPosList(param.get(1));
+
+		ArrayList<Pos> newPosList = new ArrayList<Pos>();
+		newPosList.addAll(posList1);
+		newPosList.addAll(posList2);
+
+		return new ValuePosList(newPosList);
+	}
+}
---
+++ src/com/github/novisoftware/patternDraw/geometricLanguage/lang/functions/def/pos/PosZip.java
@@ -0,0 +1,76 @@
+package com.github.novisoftware.patternDraw.geometricLanguage.lang.functions.def.pos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.github.novisoftware.patternDraw.core.CaliculateException;
+import com.github.novisoftware.patternDraw.core.langSpec.functions.FunctionDefInterface;
+import com.github.novisoftware.patternDraw.core.langSpec.typeSystem.Value;
+import com.github.novisoftware.patternDraw.core.langSpec.typeSystem.Value.ValueType;
+import com.github.novisoftware.patternDraw.core.langSpec.typeSystem.ValuePosList;
+import com.github.novisoftware.patternDraw.geometricLanguage.entity.Pos;
+import com.github.novisoftware.patternDraw.geometricLanguage.lang.InstructionRenderer;
+
+public class PosZip implements FunctionDefInterface {
+	public static final String NAME = "pos_zip";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "混ぜる";
+	}
+
+	@Override
+	public String getDescription() {
+		return "2つの座標の系列を混ぜます。";
+	}
+
+	@Override
+	public ValueType[] getParameterTypes() {
+		ValueType[] ret = {ValueType.POS_LIST, ValueType.POS_LIST};
+		return ret;
+	}
+
+	@Override
+	public String[] getParameterNames() {
+		String[] ret = {"positions1", "positions2"};
+		return ret;
+	}
+
+	@Override
+	public String[] getParameterDescs() {
+		String[] ret = {"系列1", "系列2"};
+		return ret;
+	}
+
+	@Override
+	public ValueType getReturnType() {
+		return ValueType.POS_LIST;
+	}
+
+	@Override
+	public Value exec(List<Value> param, InstructionRenderer t) throws CaliculateException {
+		ArrayList<Pos> posList1 = Value.getPosList(param.get(0));
+		ArrayList<Pos> posList2 = Value.getPosList(param.get(1));
+
+		Iterator<Pos> itr1 = posList1.iterator();
+		Iterator<Pos> itr2 = posList2.iterator();
+
+		ArrayList<Pos> newPosList = new ArrayList<Pos>();
+		while (itr1.hasNext() || itr2.hasNext()) {
+			if (itr1.hasNext()) {
+				newPosList.add(itr1.next());
+			}
+			if (itr2.hasNext()) {
+				newPosList.add(itr2.next());
+			}
+		}
+
+		return new ValuePosList(newPosList);
+	}
+}
