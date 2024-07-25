73c511c58f65c74e4003df89d5954b9f94bc947f
novisoftware <sh.sado@gmail.com>
2024-07-26 05:32:55 +0900

    オブジェクトのtoString()の作り込み

--- src/com/github/novisoftware/patternDraw/core/langSpec/typeSystem/ValueColor.java
+++ src/com/github/novisoftware/patternDraw/core/langSpec/typeSystem/ValueColor.java
@@ -8,7 +8,7 @@ public class ValueColor extends Value {
 
 	@Override
 	public String toString() {
-		return internal.toString();
+		return ValueColor.colorToString(internal);
 	}
 
 	private java.awt.Color internal;
@@ -22,5 +22,17 @@ public class ValueColor extends Value {
 			return false;
 		}
 		return this.internal.equals((java.awt.Color)o);
+	}
+	
+	/**
+	 * @param color
+	 * @return HTMLカラー形式の文字列に変換
+	 */
+	public static String colorToString(java.awt.Color color) {
+		if ((color.getAlpha() & 0xFF) == 0xFF) {
+			return String.format("#%06X", (color.getRGB() & 0xFFFFFF));
+		} else {
+			return String.format("#%02X%06X", (color.getAlpha() & 0xFF), (color.getRGB() & 0xFFFFFF));
+		}
 	}
 }
